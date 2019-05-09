e39535d12355790f66f73f7114051fa844750e84
MEERIE <league@WTS14-4.attlocal.net>
2019-05-08 17:57:04 -0700

    I FINISH MOULE 0

---
+++ src/EverythingIsAwesome.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class EverythingIsAwesome {
+	public static void main(String[] args) {
+		String name = JOptionPane.showInputDialog( "who do you like, ");
+		String billybonjoeyouletsgo = JOptionPane.showInputDialog( "I LIKE  " + name  
+				+ " TOO, I CAN TELL WE BOTH HAVE A TASTE FOR . . .  ! ! ! ! !");
+		JOptionPane.showMessageDialog (null,"FAT PEOPLE");
+	}
+
+}
---
+++ src/KnockKnock.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class KnockKnock {
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null,"knock knock");
+		JOptionPane.showMessageDialog(null,"my name is joe");
+
+	}
+}
